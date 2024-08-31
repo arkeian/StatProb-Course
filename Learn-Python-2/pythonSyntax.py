413d8c507d7a44fcc7485463f8005c59b0d5de44
Arkeian <rakhahananditya4521@gmail.com>
2024-09-01 01:27:44 +0700

    Added codes for Module 1: Python Syntax

---
+++ Learn-Python-2/pythonSyntax.py
@@ -0,0 +1,84 @@
+# Page 1
+print("Syukuri apa yang ada, hidup adalah anugerah.\nTetap jalani hidup ini, melakukan yang terbaik.")
+
+# Page 2
+print("Tak ada manusia yang terlahir sempurna.\nJangan kau sesali segala yang telah terjadi.")
+
+# Page 3
+print("Hello " + "Arkeian")
+
+# Page 4
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
+
+# Page 5
+todays_date = "August 31, 2024"
+
+# Page 6
+num1, num2 = 3, 4
+product = num1 * num2
+remainder = 1398 % 11
+
+# Page 7
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
+
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+october_rainfall = 7.20
+november_rainfall = 5.06
+december_rainfall = 4.06
+annual_rainfall += september_rainfall + october_rainfall + november_rainfall + december_rainfall
+
+# Page 8
+city_name = "St. Potatosburg"
+
+# Variable containing the population of St. Potatosburg as an integer
+city_pop = 340000
+
+# Page 9
+cucumbers = 100
+price_per_cucumber = 3.25
+total_cost = price_per_cucumber * cucumbers
+print(total_cost)
+
+# Page 10
+cucumbers, num_people = 100, 6
+whole_cucumbers_per_person = cucumbers / num_people
+print(whole_cucumbers_per_person)
+
+float_cucumbers_per_person = float(cucumbers) / num_people
+print(float_cucumbers_per_person)
+
+# Page 11
+haiku = "The old pond,\nA frog jumps in:\nPlop!"
+haiku ="""
+The old pond,
+A frog jumps in:
+Plop!
+"""
+
+# Page 12
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+age_is_12 = False
+name_is_maria = True
+
+# Page 13
+float_1 = 0.25
+float_2 = 40.0
+product = float_1 * float_2
+big_string = "The product was " + str(product) 
+
+# Page 14
+skill_completed = "Python Syntax"
+#The amount of points for each exercise may change, because points don't exist yet
+exercises_completed, points_per_exercise, point_total = 13, 5, 100
+point_total += exercises_completed * points_per_exercise
+print("I got " + str(point_total) + " points!")
