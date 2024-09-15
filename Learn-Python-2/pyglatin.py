cda1b0b06e98329ed7c10c8cfa3c5a7811636d18
Arkeian <rakhahananditya4521@gmail.com>
2024-09-15 21:23:40 +0700

    Add files for Module 5: Pyglatin

---
+++ Learn-Python-2/pyglatin.py
@@ -0,0 +1,102 @@
+# Page 1
+"""
+Tidak ada kode untuk Page 1 di sini karena belum
+ditugaskan untuk membuat sebuah blok kode.
+"""
+
+# Page 2
+print("Pig Latin")
+
+# Page 3
+print('Welcome to the Pig Latin Translator!')
+
+# Start coding here!
+original = input("Enter a word: ")
+
+# Page 4
+print('Welcome to the Pig Latin Translator!')
+
+# Start coding here!
+original = input("Enter a word: ")
+
+if len(original) > 0:
+  print(original)
+else:
+  print("empty")
+
+# Page 5
+print('Welcome to the Pig Latin Translator!')
+
+# Start coding here!
+original = input("Enter a word: ")
+
+if len(original) > 0 and original.isalpha():
+  print(original)
+else:
+  print("empty")
+
+# Page 6
+"""
+Tidak ada kode untuk Page 6 di sini karena hanya
+ditugaskan untuk uji coba blok kode Page 5.
+"""
+
+# Page 7
+pyg = "ay"
+
+# Page 8
+pyg = 'ay'
+
+original = input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+  print(original)
+else:
+  print('empty')
+
+word = original.lower()
+first = word[0]
+
+# Page 9
+pyg = 'ay'
+
+original = input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+  print(original)
+else:
+  print('empty')
+
+word = original.lower()
+first = word[0]
+new_word = "%s%s%s" % (word, first, pyg)
+
+# Page 10
+pyg = 'ay'
+
+original = input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+  print(original)
+else:
+  print('empty')
+
+word = original.lower()
+first = word[0]
+new_word = "%s%s%s" % (word[1:len(word)], first, pyg)
+
+# Page 11
+pyg = 'ay'
+
+original = input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+  print(original)
+else:
+  print('empty')
+
+word = original.lower()
+first = word[0]
+new_word = "%s%s%s" % (word, first, pyg)
+
+print(new_word)
